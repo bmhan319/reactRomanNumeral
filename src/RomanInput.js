cc5ef4c73823496c12702b4a3e8b2bf4b47794f4
Brian Han <bmhan319@hotmail.com>
2019-08-19 23:45:02 -0700

    created initial input jsx

--- src/RomanInput.js
+++ src/RomanInput.js
@@ -3,7 +3,13 @@ import React, {Component} from 'react';
 class RomanInput extends Component {
   render() {
     return(
-      <div>Hi</div>
+      <div className="input-container">
+        <form className="form1" id="form1">
+          <label htmlFor="digits" className="digits-label">Please enter a number: </label>
+          <input type="number" name="digits" id="digits" className="digits text"/>
+        </form>
+        <button className="digits-button" type="button" form="form1">Submit</button>
+      </div>
     )
   }
 }

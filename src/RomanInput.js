b8e90707859344a64508339aaddded1aa4eef6db
Brian Han <bmhan319@hotmail.com>
2019-08-19 23:59:37 -0700

    created onChange and OnSubmit functions for Input

--- src/RomanInput.js
+++ src/RomanInput.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
 
 class RomanInput extends Component {
+  state = {
+    westernNum: "0"
+  }
+
+  changeValue = (event) => {
+    this.setState({
+      westernNum: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    //run function from Apps.js
+  }
+
   render() {
     return(
       <div className="input-container">
-        <form className="form1" id="form1">
+        <form className="form1" id="form1" onSubmit={this.handleSubmit}>
           <label htmlFor="digits" className="digits-label">Please enter a number: </label>
-          <input type="number" name="digits" id="digits" className="digits text"/>
+          <input type="number" name="digits" id="digits" className="digits text" min="0" max="10000" placeholder="Up to 10,000" onChange={this.changeValue}/>
         </form>
-        <button className="digits-button" type="button" form="form1">Submit</button>
+        <button className="digits-button" type="button" form="form1" onClick={this.handleSubmit}>Submit</button>
       </div>
     )
   }
